c8bf26e5b00809f461b01593bfefd69f9f7c5f44
beboopr <linnets-router.09@icloud.com>
2023-01-28 21:27:04 -0500

    changes

--- api/index.js
+++ api/index.js
@@ -1,24 +1,24 @@
-import { MongoClient, ObjectId } from 'mongodb';
-import { uri, dev } from './credentials.js';
-import express from "express";
-import cors from "cors";
-import "dotenv/config";
+// import { MongoClient, ObjectId } from 'mongodb';
+// import { uri, dev } from './credentials.js';
+// import express from "express";
+// import cors from "cors";
+// import "dotenv/config";
 
 
-const PORT = 3030
-const app = express();
-app.use(cors());
-app.use(express.json());
+// const PORT = 3030
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
  
 //connects to DB 'shelly'
 // const client = new MongoClient(uri); // ONLINE
-const client = new MongoClient(dev); //OFFLINE
+// const client = new MongoClient(dev); //OFFLINE
 
-const db = client.db('shelly');
+// const db = client.db('shelly');
 //----------------------------------------------------------
 
 //retrieves collection item from DB 
-const shellyCollection = db.collection('shelly');
+// const shellyCollection = db.collection('shelly');
 
 
 //retrieves one line from DB
@@ -28,18 +28,42 @@ const shellyCollection = db.collection('shelly');
 //   }
 
 // ADD ONE
-const item = {
-    name: 'Ergo Chair',
-    brand: 'Autonomous',
-    color: 'Blue',
-    price: 500.00,
-    warranty: '2 years',
-}
+// const item = {
+//     name: 'Ergo Chair',
+//     brand: 'Autonomous',
+//     color: 'Blue',
+//     price: 500.00,
+//     warranty: '2 years',
+// }
   
-async function addOneItem(startup_log) {
-    const res = await db.collection("startup_log").insertOne(startup_log);
-    console.log(res);
-}
-addOneItem(item);
+// async function addOneItem(startup_log) {
+//     const res = await db.collection("startup_log").insertOne(startup_log);
+//     console.log(res);
+// }
+// addOneItem(item);
 
-app.listen(PORT, () => console.log(`api running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`api running on port ${PORT}`));
+
+import fetch from "node-fetch";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv"
+import { addNewDevice, getAllDevices, getDeviceData, toggleSwitch } from "./devices.js";
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const PORT = 3031
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}...`))
+
+app.post('/devices', addNewDevice)
+app.get('/devices', getAllDevices)
+app.get('/fetch', async (req, res) => {
+    toggleSwitch()
+    const result = await getDeviceData()
+    const data = await result.json()
+    res.send(data)
+})
