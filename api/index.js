4fcc3f5faa7187ca43cf0ef07cdff70e2aaa5a3a
beboopr <linnets-router.09@icloud.com>
2023-01-30 15:52:30 -0500

    setting up api with db, collection and port

--- api/index.js
+++ api/index.js
@@ -1,25 +1,25 @@
-// import { MongoClient, ObjectId } from 'mongodb';
-// import { uri, dev } from './credentials.js';
-// import express from "express";
-// import cors from "cors";
-// import "dotenv/config";
+import { MongoClient, ObjectId } from 'mongodb';
+import { uri, dev } from './credentials.js';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
 
-// const PORT = 3030
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
+const PORT = 3030
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-//connects to DB 'shelly'
-// const client = new MongoClient(uri); // ONLINE
+// connects to DB 'shelly'
+const client = new MongoClient(uri); // ONLINE
 // const client = new MongoClient(dev); //OFFLINE
 
-// const db = client.db('shelly');
-//----------------------------------------------------------
+const db = client.db('shelly');
+// ----------------------------------------------------------
 
-//retrieves collection item from DB
-// const shellyCollection = db.collection('shelly');
+// retrieves collection item from DB
+const shellyCollection = db.collection('shelly');
 
-//retrieves one line from DB
+// retrieves one line from DB
 // export async function getAll(req, res) {
 //     const res = await db.collection("startup-log").find().toArray()
 //     res.send(res)
@@ -40,35 +40,35 @@
 // }
 // addOneItem(item);
 
-// app.listen(PORT, () => console.log(`api running on port ${PORT}`));
+app.listen(PORT, () => console.log(`api running on port ${PORT} Captain...`));
 
-import fetch from "node-fetch";
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import {
-  addNewDevice,
-  getAllDevices,
-  getDeviceData,
-  toggleSwitch,
-} from "./devices.js";
+// import fetch from "node-fetch";
+// import express from "express";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import {
+//   addNewDevice,
+//   getAllDevices,
+//   getDeviceData,
+//   toggleSwitch,
+// } from "./devices.js";
 
-dotenv.config();
+// dotenv.config();
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
 
-const PORT = 3030;
-app.listen(PORT, () =>
-  console.log(`Listening on http://localhost:${PORT} Captain ED...`)
-);
+// const PORT = 3030;
+// app.listen(PORT, () =>
+//   console.log(`Listening on http://localhost:${PORT} Captain ED...`)
+// );
 
-app.post("/devices", addNewDevice);
-app.get("/devices", getAllDevices);
-app.get("/fetch", async (req, res) => {
-  toggleSwitch();
-  const result = await getDeviceData();
-  const data = await result.json();
-  res.send(data);
-});
+// app.post("/devices", addNewDevice);
+// app.get("/devices", getAllDevices);
+// app.get("/fetch", async (req, res) => {
+//   toggleSwitch();
+//   const result = await getDeviceData();
+//   const data = await result.json();
+//   res.send(data);
+// });
